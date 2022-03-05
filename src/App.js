8a76cd6543ccaa0c4d4d4543ec50878303a80300
carl <xiequantai@qq.com>
2022-03-05 17:19:05 +0800

    配置路由

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { BrowserRouter as Router, Redirect, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Login from "./pages/Login";
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <Router>
+      <Redirect from="/" to="/login" exact />
+      <Route path="/login" component={Login} />
+      <Route path="/layout" component={Layout} />
+    </Router>
+  );
 }
 
 export default App;
