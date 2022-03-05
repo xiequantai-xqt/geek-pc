853718b2b382a67b46cf4ae7d1d0fd7e1967a20a
carl <xiequantai@qq.com>
2022-03-05 17:28:56 +0800

    登录页面静态

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,8 +1,32 @@
-import { Card } from "antd";
+import { Button, Card, Checkbox, Form, Input } from "antd";
+import logo from "../../assets/logo.png";
+import "./index.css";
 export default function Login() {
   return (
     <div className="login">
-      <Card className="login-container">卡片组件</Card>
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        {/* 表单 */}
+        <Form size="large">
+          <Form.Item name="mobile">
+            <Input placeholder="请输入手机号" />
+          </Form.Item>
+
+          <Form.Item name="code">
+            <Input placeholder="请输入验证码" />
+          </Form.Item>
+
+          <Form.Item name="agree" valuePropName="checked">
+            <Checkbox>我已阅读并同意[用户协议]和[隐私条款]</Checkbox>
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
     </div>
   );
 }
