c988418b606710f4fc9b9ec93cec81e0b2978570
carl <xiequantai@qq.com>
2022-03-10 21:11:59 +0800

    中文化

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
+import { ConfigProvider } from "antd";
 import "antd/dist/antd.css";
+import locale from "antd/lib/locale/zh_CN";
+import "moment/locale/zh-cn";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ConfigProvider locale={locale}>
+    <App />
+  </ConfigProvider>,
+  document.getElementById("root")
+);
