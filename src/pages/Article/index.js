f7a2c15270c59c16003a05403a2d752f0a522492
carl <xiequantai@qq.com>
2022-03-10 21:09:46 +0800

    文章列表的头部静态

--- src/pages/Article/index.js
+++ src/pages/Article/index.js
@@ -1,3 +1,57 @@
+import {
+  Breadcrumb,
+  Button,
+  Card,
+  DatePicker,
+  Form,
+  Radio,
+  Select,
+} from "antd";
+import { Link } from "react-router-dom";
+
 export default function Article() {
-  return <div>文章管理</div>;
+  return (
+    <div className="root">
+      <Card
+        title={
+          <Breadcrumb separator="/">
+            <Breadcrumb.Item>
+              <Link to="/home">首页</Link>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item>文章列表</Breadcrumb.Item>
+          </Breadcrumb>
+        }
+      >
+        <Form>
+          <Form.Item label="状态" name="status">
+            <Radio.Group>
+              <Radio value={-1}>全部</Radio>
+              <Radio value={0}>草稿</Radio>
+              <Radio value={1}>待审核</Radio>
+              <Radio value={2}>审核通过</Radio>
+              <Radio value={3}>审核失败</Radio>
+            </Radio.Group>
+          </Form.Item>
+
+          <Form.Item label="频道">
+            <Select placeholder="请选择频道" style={{ width: 200 }}>
+              <Select.Option value="jack">Jack</Select.Option>
+              <Select.Option value="lucy">Lucy</Select.Option>
+              <Select.Option value="Yiminghe">yiminghe</Select.Option>
+            </Select>
+          </Form.Item>
+
+          <Form.Item label="日期">
+            <DatePicker.RangePicker />
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              筛选
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  );
 }
